38152b406f1ce84e116350c59694f24ff859792b
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-11-26 01:24:11 +0000

    feat: data generator interface

---
+++ pkg/agents/datagen.go
@@ -0,0 +1,45 @@
+package agents
+
+import (
+	"math/rand"
+
+	"github.com/gagansingh894/InferFlux/types"
+)
+
+type DataGen map[string]interface{}
+
+type Generator interface {
+	GenerateWithRandomSize(modelSpec types.ModelSpec) DataGen
+	GenerateWithFixedSize(modelSpec types.ModelSpec, size int) (DataGen, error)
+	GenerateWithinRange(modelSpec types.ModelSpec, lowerBound int, higherBound int) (DataGen, error)
+}
+
+func generateStringData(values []string, size int) []string {
+	out := make([]string, size)
+
+	for i := range out {
+		out[i] = values[rand.Intn(size)]
+	}
+
+	return out
+}
+
+func generateFloatData(mean float64, std float64, size int) []float64 {
+	out := make([]float64, size)
+
+	for i := range out {
+		out[i] = rand.NormFloat64()*std + mean
+	}
+
+	return out
+}
+
+func generateIntegerData(mean float64, std float64, size int) []int64 {
+	out := make([]int64, size)
+
+	for i := range out {
+		out[i] = int64(rand.NormFloat64()*std + mean)
+	}
+
+	return out
+}
