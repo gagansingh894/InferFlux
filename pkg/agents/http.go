cb5e740e882eb92982d794cab1e6b92eb429b870
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-11-26 01:24:17 +0000

    feat: implement data generator interface

--- pkg/agents/http.go
+++ pkg/agents/http.go
@@ -1,7 +1,102 @@
 package agents
 
+import (
+	"errors"
+	"math/rand"
+
+	"github.com/gagansingh894/InferFlux/types"
+)
+
 type HTTPAgent struct{}
 
 func NewHTTPAgent() (*HTTPAgent, error) {
 	return &HTTPAgent{}, nil
+}
+
+func (a *HTTPAgent) GenerateWithRandomSize(modelSpec types.ModelSpec) DataGen {
+	out := make(DataGen)
+	size := rand.Intn(1500) + 1
+
+	for key, value := range modelSpec {
+		switch value.Dtype {
+		case types.String:
+			out[key] = generateStringData(value.Constraint.StringConstraint.Values, size)
+		case types.Int:
+			out[key] = generateIntegerData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				size,
+			)
+		case types.Float:
+			out[key] = generateFloatData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				size,
+			)
+		}
+	}
+
+	return out
+}
+
+func (a *HTTPAgent) GenerateWithFixedSize(modelSpec types.ModelSpec, size int) (DataGen, error) {
+	out := make(DataGen)
+
+	if size <= 0 {
+		return nil, errors.New("size must be greater than zero")
+	}
+
+	for key, value := range modelSpec {
+		switch value.Dtype {
+		case types.String:
+			out[key] = generateStringData(value.Constraint.StringConstraint.Values, size)
+		case types.Int:
+			out[key] = generateIntegerData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				size,
+			)
+		case types.Float:
+			out[key] = generateFloatData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				size,
+			)
+		}
+	}
+
+	return out, nil
+}
+
+func (a *HTTPAgent) GenerateWithinRange(modelSpec types.ModelSpec, lowerBound int, upperBound int) (DataGen, error) {
+	out := make(DataGen)
+
+	if lowerBound <= 0 {
+		return nil, errors.New("lower bound must be greater than zero")
+	}
+
+	if upperBound <= lowerBound {
+		return nil, errors.New("higher bound must be greater than lower")
+	}
+
+	for key, value := range modelSpec {
+		switch value.Dtype {
+		case types.String:
+			out[key] = generateStringData(value.Constraint.StringConstraint.Values, upperBound)[lowerBound:upperBound]
+		case types.Int:
+			out[key] = generateIntegerData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				upperBound,
+			)[lowerBound:upperBound]
+		case types.Float:
+			out[key] = generateFloatData(
+				value.Constraint.NumericConstraint.Mean,
+				value.Constraint.NumericConstraint.StandardDeviation,
+				upperBound,
+			)[lowerBound:upperBound]
+		}
+	}
+
+	return out, nil
 }
